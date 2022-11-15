8a355a34cdcd18cf6a4dda32ed95aa634a399b46
adriandlj <ad1592@georgetown.edu>
2022-11-14 21:17:59 -0500

    Challenge C commit and updating unemployment.py file

--- app/unemployment.py
+++ app/unemployment.py
@@ -9,36 +9,45 @@ from dotenv import load_dotenv # <--- ADDITION
 
 load_dotenv() # <--- ADDITION
 
+from plotly.express import line
+
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
+@@ -23,6 +24,39 @@
+# Display the unemployment rate using a percent sign.
+
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
-
 response = requests.get(request_url)
-
 parsed_response = json.loads(response.text)
-#print(type(parsed_response))
-#pprint(parsed_response)
+
+
+print(type(parsed_response))
+print(parsed_response)
 
 # Challenge A
 #
 # What is the most recent unemployment rate? And the corresponding date?
-# Display the unemployment rate using a percent sign.
 
-#breakpoint()
+# breakpoint()
 
-latest = parsed_response["data"][0]
 print(latest)
-
+print("-------------------------")
+latest = parsed_response["data"][0]
+print("LATEST UNEMPLOYMENT RATE:", latest)
 
 # Challenge B
-#
+# 
 # What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
+from statistics import mean
+
+data = parsed_response["data"]
 
 this_year = [d for d in data if "2022-" in d["date"]]
 
 rates_this_year = [float(d["value"]) for d in this_year]
 #print(rates_this_year)
-
 
 print("-------------------------")
 print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
@@ -46,9 +55,12 @@ print("NO MONTHS:", len(this_year))
 
 
 # Challenge C
-#
+# 
 # Plot a line chart of unemployment rates over time.
 
+from plotly.express import line
+
+print("-------------------------")
 dates = [d["date"] for d in data]
 rates = [float(d["value"]) for d in data]
 
